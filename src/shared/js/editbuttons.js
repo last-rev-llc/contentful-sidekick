e70b3c89adcda4e6fb405c66d945d3407d073bb8
Dano Alexander <dano.williams@optimizely.com>
2019-02-12 15:25:52 -0800

    Active element styles

--- src/shared/js/editbuttons.js
+++ src/shared/js/editbuttons.js
@@ -4,7 +4,7 @@ $(() => {
   const CONTENTFUL_ENVIRONMENT = $('[name="contentful_environment"]').attr('content');
 
   const getEditBox = (elementId) => {
-    return `<a class="csk-button csk-edit-button icons icon-pencil" href="https://app.contentful.com/spaces/${CONTENTFUL_CURRENT_SPACE_ID}/environments/${CONTENTFUL_ENVIRONMENT}/entries/${elementId}" target="_blank">Edit</a>`;
+    return `<a class="csk-button csk-edit-button icons icon-pencil" href="https://app.contentful.com/spaces/${CONTENTFUL_CURRENT_SPACE_ID}/environments/${CONTENTFUL_ENVIRONMENT}/entries/${elementId}" target="_blank"></a>`;
   }
 
   if (CONTENTFUL_CURRENT_SPACE_ID && CONTENTFUL_ENVIRONMENT) {
