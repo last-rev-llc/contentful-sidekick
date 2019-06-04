d811feacf9cdb66eb1e1eadbfd5caa7e2470e8fe
Brad Taylor <bradtaylorsf@gmail.com>
2019-06-04 12:04:04 -0700

    new edits

--- src/shared/js/content.js
+++ src/shared/js/content.js
@@ -13,18 +13,22 @@ $(() => {
       const CONTENTFUL_CURRENT_SPACE_ID = $('[name="contentful_space"]').attr('content');
       const CONTENTFUL_ENVIRONMENT = $('[name="contentful_environment"]').attr('content');
 
-      const getEditBox = (elementId) => {
-        return `<a class="csk-button csk-edit-button"
-        href="https://app.contentful.com/spaces/${CONTENTFUL_CURRENT_SPACE_ID}/environments/${CONTENTFUL_ENVIRONMENT}/entries/${elementId}"
-        target="_blank"></a>`;
+      const getContentfulUrl = (elementId) => {
+        return `https://app.contentful.com/spaces/${CONTENTFUL_CURRENT_SPACE_ID}/environments/${CONTENTFUL_ENVIRONMENT}/entries/${elementId}`;
       };
 
       if (CONTENTFUL_CURRENT_SPACE_ID && CONTENTFUL_ENVIRONMENT) {
         $.each($('[data-csk-entry-id]'), (index, el) => {
           // Get the data for each element
           const elementId = $(el).data('csk-entry-id');
-          $(el).addClass('csk-element');
-          $(el).prepend(getEditBox(elementId));
+          $(el)
+            .addClass('csk-element')
+            .off('click')
+            .on('click', (e) => {
+              if ($(e.target).data('csk-entry-id') !== elementId) return;
+              e.stopPropagation();
+              window.open(getContentfulUrl(elementId));
+            });
         });
       }
     }
