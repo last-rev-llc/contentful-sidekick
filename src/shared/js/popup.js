a4cbfb5b45f57870b844d5925d5f3e4e592550a2
Adam Harris <adam@tharrisconsultants.com>
2019-10-27 09:04:18 -0700

    updated core styles for hover, blur, and background color additions so would show above overlay. updated to use background.js for turning features on/off

--- src/shared/js/popup.js
+++ src/shared/js/popup.js
@@ -9,7 +9,7 @@ const loadStorage = () => {
   chrome.storage.sync.get({
     sideKickEnabled: false,
   }, (items) => {
-    sideKickEnabled = items['sideKickEnabled'];
+    sideKickEnabled = items.sideKickEnabled;
     $('#sideKickEnabled').prop('checked', sideKickEnabled);
   });
 };
@@ -21,5 +21,24 @@ $(() => {
 $('#sideKickEnabled').on('change', (el) => {
   chrome.storage.sync.set({
     sideKickEnabled: $(el.currentTarget).is(':checked'),
+  });
+
+  chrome.tabs.getSelected(null, (tab) => {
+    const code = 'window.location.reload();';
+    chrome.tabs.executeScript(tab.id, { code });
+
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      if ($(el.currentTarget).is(':checked')) {
+        chrome.tabs.executeScript(
+          tabs[0].id,
+          { code: 'document.body.addAttribute("data-init-csk")' },
+        );
+      } else {
+        chrome.tabs.executeScript(
+          tabs[0].id,
+          { code: 'document.body.removeAttribute("data-init-csk")' },
+        );
+      }
+    });
   });
 });
