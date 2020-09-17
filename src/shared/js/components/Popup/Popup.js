47922180fe48c7242542262d2aca38b58b66d1e6
Dano Alexander <altruity@gmail.com>
2020-09-17 19:16:59 +0300

    Grab version from pkg json

--- src/shared/js/components/Popup/Popup.js
+++ src/shared/js/components/Popup/Popup.js
@@ -3,6 +3,8 @@ import getIsSideKickEnabledFromStorage from '../../helpers/getIsSideKickEnabledF
 import setSideKickEnabledInStorage from '../../helpers/setSideKickEnabled';
 import './Popup.css';
 
+const { version } = require('../../../../../package.json');
+
 function Popup() {
   const [sideKickEnabled, setSideKickEnabled] = useState(false);
   const [loaded, setLoaded] = useState(false);
@@ -39,7 +41,7 @@ function Popup() {
         <div>Is Enabled: {sideKickEnabled ? 'Yes' : 'No'}</div>
       </main>
       <footer>
-        <small className="version">v0.0.9</small>
+        <small className="version">v{version}</small>
       </footer>
     </>
   );
